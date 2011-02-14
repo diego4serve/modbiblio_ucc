c1d2fe21812143f13dd6b109c6476ffc1d35e69b
Diego Dominguez <diegofernandodominguez@gmail.com>
2011-02-14 03:52:18 -0500

    Commit Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ModbiblioUcc::Application.config.secret_token = 'f93b9e1096a26933a8cdd8479a686fee259ed174a30aff5eebcedb362d9390bbb9047ef238f286f4fce6d5b8f2f59a8149c355a44da13960363a86c28e3540a9'
